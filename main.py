2644f46d4aebe23cd9d147a56bfbccb68b49278c
crimera <stevendavemiranda2@gmail.com>
2023-09-02 16:19:50 +0800

    add task option

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from yabe import Yabe
 import argparse
 import urllib.parse
 
-model = Yabe("tiny", task="transcribe")
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--lang", type=str, default="ja", help="source language")
+parser.add_argument("--task", type=str, default="translate", help="task translate | transcribe")
 parser.add_argument("input_or_url", type=str, help="the file to transcribe")
 
 args = parser.parse_args()
 
 inpt = args.input_or_url
 lang = args.lang
+task = args.task
+
+model = Yabe("tiny", task=task)
 
 host = urllib.parse.urlparse(inpt).netloc
 
