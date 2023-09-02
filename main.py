231bd9fa7454d483e053150b1ace1a8d49d21414
crimera <stevendavemiranda2@gmail.com>
2023-09-02 21:15:51 +0800

    add yt support

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+from utils import is_supported
 from yabe import Yabe
 import argparse
 import urllib.parse
+from yt_dlp import YoutubeDL
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -19,10 +22,36 @@ model = args.model
 thumbnail = args.thumbnail
 
 model = Yabe(model, task=task)
-
 host = urllib.parse.urlparse(inpt).netloc
 
 if host == 'asmr.one':
     pass
 elif not host:
     model.transcribe_and_embed(inpt, thumbnail, lang)
+
+if is_supported(inpt):
+    print("youtube")
+
+    ydl_opts = {
+        'final_ext': 'opus',
+        'format': 'bestaudio/best',
+        'postprocessors':
+            [{
+                'format': 'jpg',
+                'key': 'FFmpegThumbnailsConvertor',
+                'when': 'before_dl'},
+             {
+                 'key': 'FFmpegExtractAudio',
+                 'nopostoverwrites': False,
+                 'preferredcodec': 'best',
+                 'preferredquality': '5'
+              }],
+        'writethumbnail': True}
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(inpt, download=True)
+        file_path = ydl.prepare_filename(info) 
+
+    filename = Path(file_path).with_suffix(".opus")
+    thumbnail = Path(file_path).with_suffix(".jpg")
+    model.transcribe_and_embed(f"{filename}", f"{thumbnail}", lang)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import yt_dlp
 
 
 def time(seconds):
@@ -6,3 +7,10 @@ def time(seconds):
             hours, rem = divmod(tdelta.seconds, 3600)
             minutes, seconds = divmod(rem, 60)
             return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
+def is_supported(url):
+    extractors = yt_dlp.extractor.gen_extractors()
+    for e in extractors:
+        if e.suitable(url) and e.IE_NAME != 'generic':
+            return True
+    return False
