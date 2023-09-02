b871667e10d979d35efe459564f31996bb2f9c9c
crimera <stevendavemiranda2@gmail.com>
2023-09-02 16:44:06 +0800

    add model flag

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument("--lang", type=str, default="ja", help="source language")
 parser.add_argument("--task", type=str, default="translate", help="task translate | transcribe")
 parser.add_argument("--thumbnail", type=str, default="", help="path to thumbnail")
+parser.add_argument("--model", type=str, help="path or size of model")
 parser.add_argument("input_or_url", type=str, help="the file to transcribe")
 
 args = parser.parse_args()
@@ -14,9 +15,10 @@ args = parser.parse_args()
 inpt = args.input_or_url
 lang = args.lang
 task = args.task
+model = args.model
 thumbnail = args.thumbnail
 
-model = Yabe("tiny", task=task)
+model = Yabe(model, task=task)
 
 host = urllib.parse.urlparse(inpt).netloc
 
